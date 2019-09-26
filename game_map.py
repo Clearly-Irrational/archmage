e8d47f2348dba67306655b2d02b1ac7e7fda0d4f
Chris Berry <chris.berry@trustid.com>
2019-09-26 14:59:00 -0700

    Created cave map cellular automata generator, still needs to have connecting of disjointed sections but otherwise works well

--- game_map.py
+++ game_map.py
@@ -16,6 +16,7 @@ class GameMap:
 
         return tiles
 
+    #Generate a traditional dungeon
     def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
         rooms = []
         num_rooms = 0
@@ -68,6 +69,32 @@ class GameMap:
                 rooms.append(new_room)
                 num_rooms += 1
 
+    #Generate a cellular automata cave system
+    def make_cave(self, map_width, map_height, player):
+        #Generator variables
+        initialy_open_chance = 40
+        number_of_extra_passes = 5
+        wall_count_threshold = 5
+
+        #First pass
+        for y in range(0, map_height):
+            for x in range(0, map_width):
+                if randint(1, 100) <= initialy_open_chance:
+                    self.tiles[x][y].blocked = False
+                    self.tiles[x][y].block_sight = False
+        for loop in range(0, number_of_extra_passes):
+            self.next_pass(map_width, map_height, wall_count_threshold)
+
+        #Place the player
+        placed = False
+        while not placed:
+            rand_x = randint(0, map_width - 1)
+            rand_y = randint(0, map_height - 1)
+            if self.tiles[rand_x][rand_y].blocked == False:
+                player.x = rand_x
+                player.y = rand_y
+                placed = True
+
     #Determine if a tile is blocked
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
@@ -96,3 +123,26 @@ class GameMap:
         for y in range(min(y1, y2), max(y1, y2) + 1):
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
+
+
+    #Next pass
+    def next_pass(self, map_width, map_height, wall_count_threshold):
+        next_pass = self
+        for y in range(0, map_height):
+            for x in range(0, map_width):
+                wall_count = 0
+                top_limit = 0 if y == 0 else 1
+                bottom_limit = 1 if y == map_height - 1 else 2
+                left_limit = 0 if x == 0 else 1
+                right_limit = 1 if x == map_width - 1 else 2
+                for y2 in range(y-top_limit, y+bottom_limit): #Note Range max is -1 by default
+                    for x2 in range(x-left_limit, x+right_limit):
+                        if self.tiles[x2][y2].blocked == True:
+                            wall_count += 1
+                if wall_count > wall_count_threshold:
+                    next_pass.tiles[x][y].blocked = True
+                    next_pass.tiles[x][y].block_sight = True
+                elif wall_count < wall_count_threshold:
+                    next_pass.tiles[x][y].blocked = False
+                    next_pass.tiles[x][y].block_sight = False
+        self = next_pass
